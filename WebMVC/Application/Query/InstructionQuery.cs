1c6eb24aab87a7103bb35e378c206865a221eeca
stank <sean.tank131@gmail.com>
2025-09-09 16:54:43 -0400

    Started Implementing the instructions. WIP

---
+++ WebMVC/Application/Query/InstructionQuery.cs
@@ -0,0 +1,9 @@
+﻿using WebMVC.Application.Query.Base;
+
+namespace WebMVC.Application.Query
+{
+    public class InstructionQuery : PagedQuery
+    {
+        public string? SearchDescription { get; set; }
+    }
+}
---
+++ WebMVC/Domain/Interfaces/Repositories/IInstructionRepository.cs
@@ -0,0 +1,10 @@
+﻿using WebMVC.Application.Query;
+using WebMVC.Domain.Entities;
+
+namespace WebMVC.Domain.Interfaces.Repositories
+{
+    public interface IInstructionRepository : IEntityRepository<Instruction>
+    {
+        Task<IEnumerable<Instruction>> GetAllAsync(IngredientQuery query);
+    }
+}
